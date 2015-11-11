69b4b8d01d55ea80a7c22d4d2784f7dd1fee8474
NinthCode <admin@ninthcode.com>
2015-11-11 16:21:13 +0800

    file add

---
+++ 20151109/test.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: NinthCode
+	> Mail: admin@ninthcode.com
+	> Created Time: 2015年11月09日 星期一 15时01分57秒
+ ************************************************************************/
+#include<stdio.h>
+int main()
+{
+	printf("Hello World\n");
+	return 0;
+}
